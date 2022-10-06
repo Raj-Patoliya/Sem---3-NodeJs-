8c41ffb3c159f5b0a7d117fa7f1c687d80aba548
Raj Patoliya <84698326+Raj-Patoliya@users.noreply.github.com>
2022-10-06 11:09:05 +0530

    Add files via upload

---
+++ 11.js
@@ -0,0 +1,21 @@
+
+console.log(8);
+
+console.log('Hola Lalaram');
+
+const x = 'Bajirao Singham';
+console.log(x);
+
+function sayName() {
+    return 'Hello Jaykant Shikre';
+}
+console.log(sayName());
+
+const name = 'Tawde';
+console.log('Hello ' + name);
+
+let obj = {
+    name: 'Gautam Bhosle',
+    age: 28
+}
+console.log(obj);
